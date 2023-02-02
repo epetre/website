241d04fffed6a03ebb022876026d3f51eda9e1f8
Emanuel <petreemanuel@gmail.com>
2023-02-02 18:18:36 -0500

    remove orphaned images when deloying docker

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,14 @@ echo '*******'
 echo '*******'
 echo 'pulling latest'
 git pull
-echo '*******'
 
+echo '*******'
 echo 'docker-compose -f docker-compose.prod.yml up -d --build'
 docker-compose -f docker-compose.prod.yml up -d --build
+
+echo '*******'
+echo "docker rmi removing unused orphaned images'
+docker rmi $(docker images -f dangling=true -q)
 echo '*******'
 echo '*******'
 echo '*******'
