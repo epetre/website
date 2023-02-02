f346f47af89168acce859fd12447eabf4ac2c617
Emanuel <petreemanuel@gmail.com>
2023-02-02 15:58:38 -0500

    wip

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Emanuel Petre Software Developer',
+  title: "Emanuel's website is a place where you can learn more about him.",
+  description: 'Emanuel Petre shocases his CV, blog and portfolio.',
+  author: 'Emanuel Petre',
   locale_region: 'en-us',
   locale: 'en',
 };
